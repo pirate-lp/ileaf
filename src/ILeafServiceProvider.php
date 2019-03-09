5d786cea2c9a5ce76ac5aec4751c197eb34a7385
Arbeit <arbeit@HS-Atelier.local>
2019-03-09 13:04:22 +0100

    - bug fix

--- src/ILeafServiceProvider.php
+++ src/ILeafServiceProvider.php
@@ -58,7 +58,8 @@ class ILeafServiceProvider extends Provider
 		{
 			if ( substr($asset_name, -4) === '.mp4' )
 			{
-				$file = '/var/www/lostideaslab/storage/' . $disk . '/' . $uri;
+				$path = Storage::disk($disk)->getAdapter()->getPathPrefix();
+				$file = $path . '/' . $uri;
 				$mime = 'video/mp4';
 				$size = filesize($file);
 				$length = $size;
